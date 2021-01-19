4e1d2b8ca27c097c30c9590067131a75ae7c0c65
dinhngocup <18520335@gm.uit.edu.vn>
2021-01-19 10:24:08 +0700

    add api add new reservation and delete reservation

---
+++ src/com/wedding/controller/ReservationApiController.java
@@ -0,0 +1,108 @@
+package com.wedding.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.wedding.dto.MonthRevenueDTO;
+import com.wedding.models.Reservation;
+import com.wedding.models.ReservationUpdate;
+import com.wedding.service.ReservationService;
+import com.wedding.utils.UrlConstant;
+
+@WebServlet({ UrlConstant.URL_RESERVATION, UrlConstant.URL_RESERVATION_ADD, UrlConstant.URL_RESERVATION_UPDATE,
+		UrlConstant.URL_RESERVATION_DELETE, UrlConstant.URL_RESERVATION_PAY, UrlConstant.URL_TOTAL_REVENUE,
+		UrlConstant.URL_MONTH_REVENUE })
+public class ReservationApiController extends HttpServlet {
+
+	private ReservationService reservationService;
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		reservationService = new ReservationService();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String servletPath = req.getServletPath();
+		switch (servletPath) {
+		case UrlConstant.URL_RESERVATION_ADD:
+			req.setCharacterEncoding("UTF-8");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html");
+			String JSON = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+			Reservation reservation = reservationService.convertJSONtoReservation(JSON);
+			reservationService.addReservation(reservation);
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json");
+
+		String servletPath = req.getServletPath();
+		String id = req.getParameter("id");
+		switch (servletPath) {
+		case UrlConstant.URL_RESERVATION:
+			if (id == null) {
+				List<ReservationUpdate> reservationUpdate = reservationService.getAllWedding();
+				Gson gson = new Gson();
+				String data = gson.toJson(reservationUpdate);
+				PrintWriter writer = resp.getWriter();
+				writer.write(data);
+				writer.flush();
+			} else {
+				int weddingID = Integer.parseInt(id);
+				ReservationUpdate reservation = reservationService.getReservationById(weddingID);
+				Gson gson = new Gson();
+				String data = gson.toJson(reservation);
+				PrintWriter writer = resp.getWriter();
+				writer.write(data);
+				writer.flush();
+			}
+			break;
+		case UrlConstant.URL_TOTAL_REVENUE:
+			int year = Integer.parseInt(req.getParameter("yearRevenue"));
+
+			List<Integer> listRevenue = reservationService.getTotalRevenue(year);
+			Gson gson = new Gson();
+			String data = gson.toJson(listRevenue);
+
+			PrintWriter writer = resp.getWriter();
+			writer.write(data);
+			writer.flush();
+			break;
+		case UrlConstant.URL_MONTH_REVENUE:
+			year = Integer.parseInt(req.getParameter("year"));
+			int month = Integer.parseInt(req.getParameter("month"));
+			List<MonthRevenueDTO> monthRevenue = reservationService.getMonthRevenue(year, month);
+			gson = new Gson();
+			data = gson.toJson(monthRevenue);
+			
+			writer = resp.getWriter();
+			writer.write(data);
+			writer.flush();
+			break;
+		default:
+			break;
+		}
+	}
+
+
+
+}
