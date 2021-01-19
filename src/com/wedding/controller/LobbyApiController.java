789bbadb4b52662371999d3286ed955e0b1cb116
ngocthinhboyy <ngocthinh1c1@gmail.com>
2021-01-19 10:16:02 +0700

    add api update/ delete lobby

--- src/com/wedding/controller/LobbyApiController.java
+++ src/com/wedding/controller/LobbyApiController.java
@@ -3,6 +3,7 @@ package com.wedding.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -56,6 +57,71 @@ public class LobbyApiController extends HttpServlet {
 			writer = resp.getWriter();
 			writer.write(data);
 			writer.flush();
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String servletPath = req.getServletPath();
+		switch (servletPath) {
+		case UrlConstant.URL_LOBBY_DELETE:
+			req.setCharacterEncoding("UTF-8");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html");
+
+			int lobbyID = Integer.parseInt(req.getParameter("id"));
+			lobbyService.deleteLobby(lobbyID);
+			;
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String servletPath = req.getServletPath();
+		switch (servletPath) {
+		case UrlConstant.URL_LOBBY_ADD:
+			req.setCharacterEncoding("UTF-8");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html");
+
+			String lobbyName = req.getParameter("lobbyName").toString();
+
+			int lobbyTypeID = Integer.parseInt(req.getParameter("lobbyTypeID"));
+
+			int maxTable = Integer.parseInt(req.getParameter("maxTable"));
+
+			Lobby lobby = new Lobby();
+			lobby.setLobbyName(lobbyName);
+			lobby.setLobbyTypeID(lobbyTypeID);
+			lobby.setMaxTable(maxTable);
+
+			lobbyService.addLobby(lobby);
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String servletPath = req.getServletPath();
+		switch (servletPath) {
+		case UrlConstant.URL_LOBBY_UPDATE:
+
+			req.setCharacterEncoding("UTF-8");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html");
+
+			String JSON = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+			Lobby lobby = lobbyService.convertJSONtoLobbyUpdate(JSON);
+
+			lobbyService.updateLobby(lobby);
 			break;
 		default:
 			break;
